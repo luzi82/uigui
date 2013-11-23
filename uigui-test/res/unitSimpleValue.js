1a408d484321089f2a516a1c6a5fa42849639c40
Luzi82 <luzi82@gmail.com>
2013-11-23 10:44:02 +0800

    add unitSimpleValue test

---
+++ uigui-test/res/unitSimpleValue.js
@@ -0,0 +1,21 @@
+(function(ug, pal, arg) {
+	
+	return {
+		// enable ignore
+		clearColor:"#11223344",
+		// child ignore
+		
+		dx:1,dy:2,
+		x0:3,x1:4,
+		y0:5,y1:6,
+		u0:7,u1:8,
+		v0:9,v1:10,
+		
+		cursor: true,
+		cursorId: "test_cur",
+		refresh: ["ref_a","ref_b","ref_c"],
+		
+		alpha: 0.5,
+	};
+
+});
