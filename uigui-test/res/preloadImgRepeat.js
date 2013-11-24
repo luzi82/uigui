c196dd56e7c5d7a3835d3b24ed91f6a31394e9f5
Luzi82 <luzi82@gmail.com>
2013-11-24 17:31:17 +0800

    add back file

---
+++ uigui-test/res/preloadImgRepeat.js
@@ -0,0 +1,21 @@
+(function(ug, pal, arg) {
+
+	return {
+		preloadImg : [ "img_a", "img_a", "img_b", "img_c", "img_d" ],
+		child : [
+	        {
+	        	img : "img_d"
+	        },
+	        {
+	        	img : "img_e"
+	        },
+	        {
+	        	img : "img_f"
+	        },
+	        {
+	        	img : "img_f"
+	        },
+		],
+	};
+
+});
